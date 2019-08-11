95d722629d2890d966329fc27b8833e73efa0ae7
Jonathan Philipps <jphilipps@mail.de>
2019-08-11 14:54:32 +0200

    implement and add a port of the antlr4-c3 unit test library to c# (based on the java port)

---
+++ ports/c#/test/Antlr4CodeCompletion.CoreUnitTest/Utils/CountingErrorListener.cs
@@ -0,0 +1,25 @@
+using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+
+namespace Antlr4CodeCompletion.CoreUnitTest.Utils
+{
+    /// <summary>
+    /// </summary>
+    /// <remarks>
+    /// Port of antlr-c3 java unit test library to c#
+    /// The c3 engine is able to provide code completion candidates useful for
+    /// editors with ANTLR generated parsers, independent of the actual
+    /// language/grammar used for the generation.
+    /// https://github.com/mike-lischke/antlr4-c3
+    /// </remarks>
+    public class CountingErrorListener : BaseErrorListener
+    {
+        public int ErrorCount = 0;
+
+        public override void SyntaxError([NotNull] IRecognizer recognizer, [Nullable] IToken offendingSymbol, int line, int charPositionInLine, [NotNull] string msg, [Nullable] RecognitionException e)
+        {
+            base.SyntaxError(recognizer, offendingSymbol, line, charPositionInLine, msg, e);
+            ErrorCount++;
+        }
+    }
+}
