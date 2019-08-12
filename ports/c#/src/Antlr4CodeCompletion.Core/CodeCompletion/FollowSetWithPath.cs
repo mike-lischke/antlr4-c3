a816db5d965750d1f5f6d02afe98962a09da8892
Jonathan Philipps <jphilipps@mail.de>
2019-08-12 18:47:48 +0200

    add debug annotations for code completion classes and override equals as well as gethashcode methods

--- ports/c#/src/Antlr4CodeCompletion.Core/CodeCompletion/FollowSetWithPath.cs
+++ ports/c#/src/Antlr4CodeCompletion.Core/CodeCompletion/FollowSetWithPath.cs
@@ -1,5 +1,7 @@
 using Antlr4.Runtime.Misc;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Antlr4CodeCompletion.Core.CodeCompletion
 {
@@ -12,10 +14,33 @@ namespace Antlr4CodeCompletion.Core.CodeCompletion
     /// language/grammar used for the generation.
     /// https://github.com/mike-lischke/antlr4-c3
     /// </remarks>
-    internal class FollowSetWithPath
+    [DebuggerDisplay("FollowSetWithPath: following = {Following} , intervals = {Intervals}, path = {Path}")]
+    internal class FollowSetWithPath : IEquatable<FollowSetWithPath>
     {
         internal IList<int> Following { get; set; }
         internal IntervalSet Intervals { get; set; }
         internal IList<int> Path { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as FollowSetWithPath);
+        }
+
+        public bool Equals(FollowSetWithPath other)
+        {
+            return other != null &&
+                   EqualityComparer<IList<int>>.Default.Equals(Following, other.Following) &&
+                   EqualityComparer<IntervalSet>.Default.Equals(Intervals, other.Intervals) &&
+                   EqualityComparer<IList<int>>.Default.Equals(Path, other.Path);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 904860839;
+            hashCode = hashCode * -1521134295 + EqualityComparer<IList<int>>.Default.GetHashCode(Following);
+            hashCode = hashCode * -1521134295 + EqualityComparer<IntervalSet>.Default.GetHashCode(Intervals);
+            hashCode = hashCode * -1521134295 + EqualityComparer<IList<int>>.Default.GetHashCode(Path);
+            return hashCode;
+        }
     }
 }
