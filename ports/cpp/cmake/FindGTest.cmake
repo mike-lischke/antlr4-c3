634cacc4a078023ef1d256660041bf74bcc2e0e6
vityaman <vityaman.dev@yandex.ru>
2024-07-24 16:01:25 +0300

    #132 Fetch GTest from repository
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

---
+++ ports/cpp/cmake/FindGTest.cmake
@@ -0,0 +1,11 @@
+include(FetchContent)
+
+FetchContent_Declare(
+  googletest
+  URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
+)
+
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(googletest)
+include(GoogleTest)
