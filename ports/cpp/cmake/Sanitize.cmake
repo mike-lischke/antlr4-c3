830e510360274b388d7155dbfc5eaae1200757a2
vityaman <vityaman.dev@yandex.ru>
2024-07-24 11:34:06 +0300

    #132 Build C++ port via CMake
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

---
+++ ports/cpp/cmake/Sanitize.cmake
@@ -0,0 +1,13 @@
+set(UBSAN_COMPILE_FLAGS -fsanitize=undefined -fno-sanitize-recover=all)
+set(UBSAN_LINK_FLAGS -fsanitize=undefined)
+if(UBSAN)
+    add_compile_options(${UBSAN_COMPILE_FLAGS})
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${UBSAN_LINK_FLAGS}")
+endif()
+
+set(ASAN_COMPILE_FLAGS -fsanitize=address,undefined -fno-sanitize-recover=all)
+set(ASAN_LINK_FLAGS -fsanitize=address,undefined)
+if(ASAN)
+    add_compile_options(${ASAN_COMPILE_FLAGS})
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${ASAN_LINK_FLAGS}")
+endif()
