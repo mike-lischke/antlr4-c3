2cac0e6aa1474c0eb4e2c270acf0feb71ee58cd0
vityaman <vityaman.dev@yandex.ru>
2024-08-07 11:02:16 +0300

    #136 Run tests in random order
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

---
+++ ports/cpp/ci/check-format.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+CLANG_FORMAT="clang-format"
+if command -v clang-format-18 > /dev/null 2>&1; then
+    CLANG_FORMAT="clang-format-18"
+fi
+
+find source test -iname '*.hpp' -o -iname '*.cpp' \
+| xargs "$CLANG_FORMAT" -Werror --dry-run \
+    --fallback-style=Google --verbose
---
+++ ports/cpp/ci/test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"/../build/test
+
+ctest
+if [ $? -ne 0 ]; then
+    cat Testing/Temporary/LastTest.log
+    exit 1
+fi
+
+set -e
+
+TESTS="
+whitebox
+expr
+cpp14
+"
+
+for test in $TESTS; do
+    for i in $(seq 5); do
+        (cd "./$test" && "./antlr4-c3-test-$test" --gtest_shuffle)
+    done
+done
